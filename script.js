68eaa59d51a776f0a282ddd6c75685f89448e0db
905695 <81233851+sweetpinkie@users.noreply.github.com>
2022-09-23 11:57:32 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,45 @@
+const button = document.querySelector('.button');
+const numbers = document.querySelectorAll('.number');
+const input = document.querySelector('.input');
+
+console.log(input)
+
+
+button.addEventListener('click', ()=>{
+    number = input.value;
+
+    
+
+    if(number >= 8){
+        number = number - 8;
+        numbers[0].innerHTML = '1';
+        }else{
+        numbers[0].innerHTML = '0';
+    }
+    
+    if(number >= 4){
+        number = number - 4;
+        numbers[1].innerHTML = '1';
+    }else{
+        numbers[1].innerHTML = '0';
+    }
+    
+    if(number >= 2){
+        number = number - 2;
+        numbers[2].innerHTML = '1';
+    }else{
+        numbers[2].innerHTML = '0'
+    }
+    
+    if(number >= 1){
+        numbers[3].innerHTML = '1';
+    }else{
+        numbers[3].innerHTML = '0'
+    }
+    
+    console.log(number);
+});
+
+
+
+
